204ed5412a7058171818fd5ca5766f50619929ea
behzadvef <behzad.vefaghi@gmail.com>
2015-08-26 11:02:59 +0200

    Authorization

--- LMS-Proj/Controllers/HomeController.cs
+++ LMS-Proj/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace LMS_Proj.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Learning Management System (LMS)";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Mail: contact@Lexicon.Lex";
 
             return View();
         }
